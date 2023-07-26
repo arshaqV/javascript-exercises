a1e27b76117f7a5ec549b1534e7c017a89a68306
arshaqV <arshaq.assoo@gmail.com>
2023-07-26 19:06:31 +0530

    Complete Odin exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (const arg of elements) {
+        do {
+        const index = array.indexOf(arg);
+if (index > -1) { 
+  array.splice(index, 1);
+}
+      }  while(array.indexOf(arg) > -1)
+    }
+      return array
 
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
